174a34eb1bf55e3ee6e021301ef94380d7329bf7
Charlie <huh4142@naver.com>
2020-10-25 10:26:54 +1100

    lesson-6 Mongoose Hooks

--- models/User.js
+++ models/User.js
@@ -22,6 +22,18 @@ const userSchema = new mongoose.Schema({
   }
 });
 
+// fire a function after doc saved to db
+userSchema.post('save', function (doc, next) {
+  console.log('new user was created & saved', doc);
+  next();
+});
+
+// fire a function before doc saved to db
+userSchema.pre('save', function (next) {
+  console.log('user about to be created', this)
+  next();
+});
+
 // model('user') <- singular
 const User = mongoose.model('user', userSchema);
 
